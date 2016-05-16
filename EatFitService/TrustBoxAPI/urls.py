196ed8800b5828a7d3e89a3bb519abd75f764773
Sven Brunner <sven.brunner@comptex.ch>
2016-05-16 12:42:02 +0200

    first model draft

--- EatFitService/TrustBoxAPI/urls.py
+++ EatFitService/TrustBoxAPI/urls.py
@@ -9,6 +9,6 @@ import views
 router = routers.DefaultRouter()
 
 urlpatterns = [
-     url(r'^$', include(router.urls)),
+     url(r'^', include(router.urls)),
      url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
