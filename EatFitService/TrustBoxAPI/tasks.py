8a74bd9e08f0fc97c52ce77f01879f0829989333
Sven Brunner <sven.brunner@comptex.ch>
2016-05-18 17:18:01 +0200

    import fix

--- EatFitService/TrustBoxAPI/tasks.py
+++ EatFitService/TrustBoxAPI/tasks.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 from celery import shared_task, task
 from celery.task.base import periodic_task
 from celery.schedules import crontab
-from trustbox_connector import *
+from TrustBoxAPI import trustbox_connector
 
 
 @shared_task
@@ -16,4 +16,4 @@ def get_trustbox_data_by_call():
 
 @periodic_task(run_every=(crontab(minute=0, hour=2)), name="get_trustbox_data")
 def get_trustbox_data():
-    load_changed_data()
+    trustbox_connector.load_changed_data()
