ca4765b7fb5ada662bd01b6367ea07d435ee0a44
Sven Brunner <sven.brunner@comptex.ch>
2016-05-31 10:30:44 +0200

    no message

--- EatFitService/TrustBoxAPI/reebate_connector.py
+++ EatFitService/TrustBoxAPI/reebate_connector.py
@@ -2,6 +2,7 @@ import requests
 from EatFitService import settings
 import xlrd
 from TrustBoxAPI.models import ProductName
+from fuzzywuzzy import fuzz
 
 def get_customer_trace():
     data = {'username':settings.REEBATE_USERNAME, "password": settings.REEBATE_PASSWORD, "lastReceiptMillis" : 1444216910000} 
@@ -13,13 +14,15 @@ def excel_trace_to_db():
     book = xlrd.open_workbook("E:\\Dropbox\\Projekt Salz\\0 Technical\\Reebate\\Klaus_Migros-Cumulus_Log.xls")
     sh = book.sheet_by_index(0)
     matches = 0
+    product_names = ProductName.objects.filter(language_code="de")
     for rx in range(sh.nrows):
-        if i < 1: #first line is excel header
+        if rx < 1: #first line is excel header
             pass
         else:
             article_name = sh.row(rx)[5]
-            product_names = ProductName.objects.filter(language_code="de", name=article_name)
-            if product_names.exists():
-                print(product_name.name)
-                matches = matches + 1
+            for product_name in product_names:
+                r = fuzz.token_set_ratio(product_name.name, article_name)
+                if r > 80:
+                    print("product name: " + str(product_name.name) + " article name: " + str(article_name))
+                    matches = matches + 1
     print("# of matches: " + str(matches))
