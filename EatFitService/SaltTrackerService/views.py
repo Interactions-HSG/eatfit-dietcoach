2551601a446d140ff0b48d1dc9f79fccb8d12ef2
Sven Brunner <sven.brunner@comptex.ch>
2016-07-05 21:10:55 +0200

    no message

--- EatFitService/SaltTrackerService/views.py
+++ EatFitService/SaltTrackerService/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
-from SaltTrackerService.models import SaltTrackerUser
+from SaltTrackerService.models import SaltTrackerUser, MigrosItem
 from SaltTrackerService import reebate_connector
 
 
 def test(request):
-    reebate_connector.fill_db()
-    return HttpResponse("")
+    #reebate_connector.fill_db()
+    migros_items_list = MigrosItem.objects.using("salttracker").all()
+    return HttpResponse("ok")
