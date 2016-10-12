4b71cf1ee2241eb6cad8c13cbc073d0d9e83b7e5
fx142 <klaus.l.fuchs@gmail.com>
2016-10-12 16:51:38 +0200

    test

--- FoodCoachWebsite/FoodCoachWebsite/FoodCoachWebsite/settings.py
+++ FoodCoachWebsite/FoodCoachWebsite/FoodCoachWebsite/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '68b19c2b-1d7b-425b-9e54-3c8dfa22a270'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
