3eeb34c0c0e6c7a22aba6ded4f0021559408207f
Sven Brunner <sven.brunner@comptex.ch>
2017-12-03 20:04:56 +0100

    updated requirements

--- EatFitService/NutritionService/urls.py
+++ EatFitService/NutritionService/urls.py
@@ -1,9 +1,13 @@
 import views
+from rest_framework import routers
 import NutritionService
 from rest_framework.authtoken import views as auth_views
-from django.conf.urls import url
+from django.conf.urls import url, include
+
+router = routers.DefaultRouter()
 
 urlpatterns = [
+     url(r'^', include(router.urls)),
      url(r'^products/(?P<gtin>\d{0,50})/$', NutritionService.views.get_product),
      url(r'^update', NutritionService.views.update_database),
 ]
