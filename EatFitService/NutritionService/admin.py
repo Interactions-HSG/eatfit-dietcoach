adcdf4512e128236ac764ccc684d5cf1a773e38a
Sven Brunner <sven.brunner@comptex.ch>
2018-01-07 14:33:55 +0100

    add allergens to product admin

--- EatFitService/NutritionService/admin.py
+++ EatFitService/NutritionService/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
-from NutritionService.models import MajorCategory, Product, MinorCategory
+from NutritionService.models import MajorCategory, Product, MinorCategory, Allergen
+
+class AllergenInline(admin.TabularInline):
+    model = Allergen
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ("product_name_de", "gtin")
     search_fields = ('product_name_de', 'gtin')
     save_as = True
+    inlines = [
+        AllergenInline,
+    ]
+
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(MajorCategory)
