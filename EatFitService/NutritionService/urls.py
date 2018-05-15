cb16066a7efe099d018e06f4d54a0948e23fa9ca
Martin Damov <martin.damov@holo-one.com>
2018-05-15 15:35:28 +0200

    Included the reporting views in the urls.

--- EatFitService/NutritionService/urls.py
+++ EatFitService/NutritionService/urls.py
@@ -2,18 +2,21 @@ import views
 from NutritionService import legacy_views
 from rest_framework import routers
 import NutritionService
-from rest_framework.authtoken import views as auth_views
 from django.conf.urls import url, include
 
 router = routers.DefaultRouter()
 
 urlpatterns = [
      url(r'^', include(router.urls)),
+
      url(r'^products/(?P<gtin>\d{0,50})/$', NutritionService.views.get_product),
+     url(r'^products/better-products/(?P<gtin>\d{0,50})/', NutritionService.views.get_better_products),
+     url(r'^product/report/$', NutritionService.views.report_product),
+     url(r'^product/missing/$', NutritionService.views.report_missing_gtin),
+
      url(r'^update', NutritionService.views.update_database),
      url(r'^products/from-openfood/', NutritionService.views.get_products_from_openfood),
      url(r'^products/calculate-ofocm/', NutritionService.views.calculate_ofcom_values),
-     url(r'^products/better-products/(?P<gtin>\d{0,50})/', NutritionService.views.get_better_products),
      url(r'^legacy/import-missing-trustbox/$', legacy_views.import_missing_products),
      url(r'^legacy/import-trustbox/$', legacy_views.import_trustbox_products),
      url(r'^legacy/import-categories/$', legacy_views.import_categories),
