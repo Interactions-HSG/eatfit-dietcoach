0abc234fb242b88c7a02e7db8dc8599b7f86db7b
madjazz <madjazz@mykolab.com>
2019-01-17 08:42:29 +0100

    Complete Docker setup, add apache config files

--- EatFitService/eatfitenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/Timo/Documents/adnexo/ETH/Test/eatfitrepo/EatFitService/eatfitenv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
