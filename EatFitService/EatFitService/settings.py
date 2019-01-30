ad56a5837684d4f9353e4015de0e77c6f663d9f3
madjazz <madjazz@mykolab.com>
2019-01-30 11:47:20 +0100

    tickets 2-4 resolved

--- EatFitService/EatFitService/settings.py
+++ EatFitService/EatFitService/settings.py
@@ -15,7 +15,7 @@ import posixpath
 from os import path
 from EatFitService import local_settings
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)s
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
