19ebda60ff0ca2624a93ede4c5b0cce236eeafa4
madjazz <madjazz@mykolab.com>
2019-02-25 11:55:14 +0100

    remove commented code

--- EatFitService/NutritionService/test_send_receipts.py
+++ EatFitService/NutritionService/test_send_receipts.py
@@ -2,8 +2,8 @@ from __future__ import print_function
 import pytest
 from model_mommy import mommy
 from .views import views
-from models import Matching, NonFoundMatching, ErrorLog, DigitalReceipt, ReceiptToNutritionUser, \
-    ReceiptToNutritionPartner, MajorCategory, MinorCategory, Product, User
+from models import NonFoundMatching, DigitalReceipt, ReceiptToNutritionUser, \
+    ReceiptToNutritionPartner, MajorCategory, MinorCategory, Product
 
 
 @pytest.mark.django_db
@@ -16,22 +16,13 @@ def test_category_logging():
     major_category = mommy.make(MajorCategory)
     minor_category = mommy.make(MinorCategory, category_major=major_category)
 
-    product = mommy.make(Product,
-                         major_category=major_category,
-                         minor_category=minor_category)
-
-    # Foreign Keys:
-    # major_category
-    # minor_category
+    mommy.make(Product,
+               major_category=major_category,
+               minor_category=minor_category)
 
     digital_receipt = mommy.make(DigitalReceipt,
                                  r2n_user=r2n_user,
                                  business_unit='Migros')
-    # match = mommy.make(Matching,
-    #                    article_id=getattr(digital_receipt, 'article_id'),
-    #                    article_type=getattr(digital_receipt, 'article_type'),
-    #                    gtin=getattr(product, 'gtin'),
-    #                    eatfit_product=product)
 
     mommy.make(NonFoundMatching, article_id=digital_receipt.article_id, article_type="Mafia")
 
