a7627f57a09d56e92ea30f1cdcb43ea482ebd66c
madjazz <madjazz@mykolab.com>
2019-03-05 10:37:54 +0100

    rebase from develop, solve merge conflicts, rewrite regex validator for market_region_id

--- EatFitService/NutritionService/validators.py
+++ EatFitService/NutritionService/validators.py
@@ -3,5 +3,5 @@ from django.core.validators import RegexValidator, MaxValueValidator, MinValueVa
 minimum_float_validator = MinValueValidator(0.0)
 maximum_float_validator = MaxValueValidator(100.0)
 
-retailer_id_validator = RegexValidator(regex=r'^[a-z]{2}.+$', message='Invalid ID pattern (Format: ch.coop)')
-market_region_id_validator = RegexValidator(regex=r'^[A-Z]{2}$', message='Invalid ID pattern (Format: CH')
+retailer_id_validator = RegexValidator(regex=r'^[\w]{2}\.[\w\d-_]+$', message='Invalid ID pattern (Format: ch.coop)')
+market_region_id_validator = RegexValidator(regex=r'^[A-Z]{2}$', message='Invalid ID pattern (Format: CH)')
