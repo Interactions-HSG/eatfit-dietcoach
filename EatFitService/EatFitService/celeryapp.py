958539fb268b8c7725628e84a4c3ef2efcba45dd
madjazz <madjazz@mykolab.com>
2019-03-25 16:48:53 +0100

    Pass broker url explicitly to app through import from local settings

--- EatFitService/EatFitService/celeryapp.py
+++ EatFitService/EatFitService/celeryapp.py
@@ -1,8 +1,9 @@
 import os
 from celery import Celery
+from local_settings import CELERY_BROKER_URL
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EatFitService.settings')
 
-app = Celery('EatFitService', broker_url='amqp://rabbitmq')
+app = Celery('EatFitService', broker_url=CELERY_BROKER_URL)
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
