be82d84a73d3df1ffbb83af0ab2922eb95eb381d
madjazz <madjazz@mykolab.com>
2019-03-25 16:53:29 +0100

    remove broker_url param and import from local settings instead

--- EatFitService/EatFitService/celeryapp.py
+++ EatFitService/EatFitService/celeryapp.py
@@ -1,9 +1,8 @@
 import os
 from celery import Celery
-from local_settings import CELERY_BROKER_URL
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EatFitService.settings')
 
-app = Celery('EatFitService', broker_url=CELERY_BROKER_URL)
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app = Celery('EatFitService')
+app.config_from_object('django.conf:local_settings', namespace='CELERY')
 app.autodiscover_tasks()
