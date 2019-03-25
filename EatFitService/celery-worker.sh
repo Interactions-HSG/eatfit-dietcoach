d4a54904e0e61ab06fb72fe32d8e6d48a122964f
Christian Fässler <christian.faessler@intersection.com>
2019-03-25 08:26:08 -0400

    Show job log as celery output

--- EatFitService/celery-worker.sh
+++ EatFitService/celery-worker.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
+mkdir -p /home/eatfit/eatfitrepo/EatFitService/logs
+touch /home/eatfit/eatfitrepo/EatFitService/logs/celery.log
+
+
 service supervisor start
 supervisorctl reread
 supervisorctl update
 supervisorctl start all
+
+tail -f /home/eatfit/eatfitrepo/EatFitService/logs/celery.log
