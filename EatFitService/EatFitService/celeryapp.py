ddf21fcde60f205bfb7ead4026c80fd8e1057480
madjazz <madjazz@mykolab.com>
2019-03-25 16:16:15 +0100

    specify broker url explicitly in Celery instance

--- EatFitService/EatFitService/celeryapp.py
+++ EatFitService/EatFitService/celeryapp.py
@@ -3,6 +3,6 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EatFitService.settings')
 
-app = Celery('EatFitService')
+app = Celery('EatFitService', broker_url='amqp://rabbitmq')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
