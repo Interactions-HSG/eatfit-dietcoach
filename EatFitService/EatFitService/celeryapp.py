be4bad330f7ba05389bbec8b64fbaa3ce8bcd5bf
madjazz <madjazz@mykolab.com>
2019-03-28 17:50:41 +0100

    Avoid circular import of settings, use local_settings as default

--- EatFitService/EatFitService/celeryapp.py
+++ EatFitService/EatFitService/celeryapp.py
@@ -1,8 +1,8 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EatFitService.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EatFitService.local_settings')
 
 app = Celery('EatFitService')
-app.config_from_object('django.conf:local_settings', namespace='CELERY')
+app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
