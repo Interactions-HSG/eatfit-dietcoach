a1378546654eaeddc9f139282bc7f8194f83937d
madjazz <madjazz@mykolab.com>
2019-04-01 17:05:07 +0200

    Change certainity to certainty in allergen import

--- EatFitService/NutritionService/data_import.py
+++ EatFitService/NutritionService/data_import.py
@@ -65,11 +65,11 @@ class AllergensImport(ImportBase):
 
         transform_form_headers = {
             'allergen_name': 'allergen_name',
-            'allergen_certainty': 'certainity',
+            'allergen_certainty': 'certainty',
         }
         transform_csv_headers = {
             'allergen_name': 'name',
-            'certainity': 'certainity'
+            'certainty': 'certainty'
         }
 
         update_headers = [transform_form_headers[key] for key, value in self.form_params.items() if value]
@@ -81,7 +81,6 @@ class AllergensImport(ImportBase):
 
                 get_row_headers = {header: row[header] for header in update_headers}
                 update_allergens = {transform_csv_headers[key]: value for key, value in get_row_headers.items()}
-
 
                 try:
                     product_object = Product.objects.get(gtin=int(row['gtin']))
