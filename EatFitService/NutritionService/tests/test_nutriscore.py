2e70660b05b7212fc0f6ffa25001128e0d1b8c4b
Timo Klingler <timo.klingler@adnexo.ch>
2019-08-15 16:30:56 +0200

    Add new tests for testing sodium and testing mixed products

--- EatFitService/NutritionService/tests/test_nutriscore.py
+++ EatFitService/NutritionService/tests/test_nutriscore.py
@@ -1,4 +1,5 @@
 from model_mommy import mommy
+import itertools
 import pytest
 
 from NutritionService.models import Product, NutritionFact
@@ -36,4 +37,37 @@ def test_data_sufficiency():
     assert test_prod.ofcom_value == 10  # 50 > argmax([values]) = 45
 
 
+@pytest.mark.django_db
+def test_sodium():
 
+    test_prod = mommy.make(Product)
+
+    test_nutrients = {'name': 'sodium', 'amount': 1.2, 'unit_of_measure': 'g', 'is_mixed': False}
+    test_prod.nutrients.create(**test_nutrients)
+    test_prod.save()
+
+    nutri_score_1 = test_prod.ofcom_value
+    assert nutri_score_1 == 10
+
+    test_nutrients = {'name': 'sodium', 'amount': 270, 'unit_of_measure': 'mg', 'is_mixed': False}
+    test_prod.nutrients.update(**test_nutrients)
+    test_prod.save()
+
+    nutri_score_2 = test_prod.ofcom_value
+    assert nutri_score_2 == 2
+    assert nutri_score_1 != nutri_score_2
+
+
+@pytest.mark.django_db
+def test_mixed():
+
+    test_prod = mommy.make(Product)
+
+    mixed = {'name': 'protein', 'amount': 10, 'unit_of_measure': 'g', 'is_mixed': True}
+    not_mixed = {'name': 'protein', 'amount': 1.5, 'unit_of_measure': 'g', 'is_mixed': False}
+
+    mommy.make(NutritionFact, product=test_prod, **mixed)
+    mommy.make(NutritionFact, product=test_prod, **not_mixed)
+
+    test_prod.save()
+    assert test_prod.ofcom_value == -5
