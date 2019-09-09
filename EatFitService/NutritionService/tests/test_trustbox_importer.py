218453b5a08a1a93a39922d8c1ec5ceeac99791e
Timo Klingler <timo.klingler@adnexo.ch>
2019-09-09 10:49:21 +0200

    Modify testing data, update existing tests and add new test for _baseUnitofMeasure and _baseAmount

--- EatFitService/NutritionService/tests/test_trustbox_importer.py
+++ EatFitService/NutritionService/tests/test_trustbox_importer.py
@@ -1,6 +1,7 @@
 import json
 from model_mommy import mommy
 import pytest
+from toolz import itertoolz
 
 from NutritionService.models import Product, NutritionFact, Ingredient, Allergen
 from NutritionService.views.views import create_product
@@ -73,6 +74,29 @@ def test_trustbox_import_automatic_update_on():
     assert test_product.product_size_unit_of_measure == 'kg'
     assert test_product.source == 'Trustbox'
     assert test_product.source_checked
-    assert NutritionFact.objects.count() == 9
+    assert NutritionFact.objects.count() == 8
     assert Ingredient.objects.count() == 4
     assert Allergen.objects.count() == 5
+
+
+@pytest.mark.django_db
+def test_trustbox_import_base_unit_of_measurement_base_amount():
+    """
+    We want to assert that _baseUnitofMeasure and _baseAmount follow a specific pattern (g/100) when creating or
+    updating data from the Trustbox importer. All data with the name "energyKcal in the testing data do not follow the
+    required pattern, therefore it should be ignored and not present in the testing Database.
+    """
+
+    assert Product.objects.count() == 0
+
+    mommy.make(Product, gtin=7610032043383, automatic_update=True)
+
+    product_processed = product['productList'][0]['products'][0]
+    create_product(product_processed)
+
+    nutrition_fact_objects = NutritionFact.objects.all()
+    nutrition_fact_objects_grouped_name = itertoolz.groupby(lambda x: x.name, list(nutrition_fact_objects))
+
+    assert Product.objects.count() == 1
+    assert NutritionFact.objects.count() == 8
+    assert 'energyKcal' not in nutrition_fact_objects_grouped_name
