023de398ff9d7f9d613d378ea6df559cada6b3cc
Timo Klingler <timo.klingler@adnexo.ch>
2020-02-07 14:53:58 +0100

    Update tests after rebase

--- EatFitService/NutritionService/tests/test_basket_analysis_integration.py
+++ EatFitService/NutritionService/tests/test_basket_analysis_integration.py
@@ -1,6 +1,7 @@
 import pytest
 from model_mommy import mommy
 
+from django.db.models import F, Func
 from django.contrib.auth.models import User
 
 from rest_framework.test import APIRequestFactory, force_authenticate
@@ -18,6 +19,7 @@ URL_REVERSE = 'basket-analysis'
 ERROR_KEY = 'error'
 ARTICLE_ID_SIMPLE = 'Apfel Braeburn'
 ARTICLE_TYPE_SIMPLE = 'Migros_long_v1'
+PRICE = 1.85
 
 errors = BasketAnalysisErrors()
 
@@ -205,9 +207,10 @@ def test_basket_analysis_api_matching_non_found_exists():
     assert response.status_code == status.HTTP_200_OK
     assert models.NonFoundMatching.objects.count() == 1
 
-    test_object = models.NonFoundMatching.objects.get(article_id=ARTICLE_ID_SIMPLE, article_type='Migros_long_v1')
+    expected_results = models.NonFoundMatching.objects.get(article_id=ARTICLE_ID_SIMPLE,
+                                                           article_type=ARTICLE_TYPE_SIMPLE)
 
-    assert test_object.counter == 1
+    assert expected_results.counter == 1
 
 
 @pytest.mark.django_db
@@ -250,9 +253,152 @@ def test_basket_analysis_api_matching_multiple():
     assert response.status_code == status.HTTP_200_OK
     assert models.NonFoundMatching.objects.count() == 0
 
-    test_object = models.Matching.objects.filter().first()
+    expected_results = models.Matching.objects.filter().first()
 
-    assert test_object.gtin == 0
+    assert expected_results.gtin == 0
+
+
+@pytest.mark.django_db
+def test_basket_analysis_api_matching_multiple_price_does_not_exist():
+    assert User.objects.count() == 0
+    assert models.ReceiptToNutritionPartner.objects.count() == 0
+    assert models.ReceiptToNutritionUser.objects.count() == 0
+    assert models.Matching.objects.count() == 0
+    assert models.NonFoundMatching.objects.count() == 0
+    assert models.Product.objects.count() == 0
+
+    user = User.objects.create_user(username=TEST_USER_AND_PASSWORD, password=TEST_USER_AND_PASSWORD)
+    r2n_partner = mommy.make(models.ReceiptToNutritionPartner, user=user, name=PARTNER_AND_USER_NAME)
+    r2n_user = mommy.make(models.ReceiptToNutritionUser, r2n_partner=r2n_partner, r2n_username=PARTNER_AND_USER_NAME)
+    test_product = mommy.make(models.Product)
+    mommy.make(models.Matching, article_id=ARTICLE_ID_SIMPLE, article_type=ARTICLE_TYPE_SIMPLE, gtin=0,
+               eatfit_product=test_product, price_per_unit=None)
+    mommy.make(models.Matching, article_id=ARTICLE_ID_SIMPLE, article_type=ARTICLE_TYPE_SIMPLE, gtin=1,
+               eatfit_product=test_product, price_per_unit=None)
+
+    BASKET_ANALYSIS_DATA_SIMPLE['r2n_partner'] = r2n_partner.name
+    BASKET_ANALYSIS_DATA_SIMPLE['r2n_username'] = r2n_user.r2n_username
+
+    assert User.objects.count() == 1
+    assert models.ReceiptToNutritionPartner.objects.count() == 1
+    assert models.ReceiptToNutritionUser.objects.count() == 1
+    assert models.Matching.objects.count() == 2
+    assert models.Product.objects.count() == 1
+
+    api_client = APIRequestFactory()
+    view = views.SendReceiptsView.as_view()
+    url = reverse(URL_REVERSE)
+    request = api_client.post(url, BASKET_ANALYSIS_DATA_SIMPLE, format='json')
+    force_authenticate(request, user=user)
+    response = view(request)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert models.NonFoundMatching.objects.count() == 0
+
+    expected_results = models.Matching.objects.filter(article_id=ARTICLE_ID_SIMPLE,
+                                                      article_type=ARTICLE_TYPE_SIMPLE,
+                                                      price_per_unit__isnull=False).annotate(
+        absolute_price_difference=Func(F('price_per_unit') - PRICE, function='ABS')).order_by(
+        'absolute_price_difference').first()
+
+    assert expected_results is None
+
+    expected_results = models.Matching.objects.filter().first()
+
+    assert expected_results.gtin == 0
+
+
+@pytest.mark.django_db
+def test_basket_analysis_api_matching_multiple_some_prices_exist():
+    assert User.objects.count() == 0
+    assert models.ReceiptToNutritionPartner.objects.count() == 0
+    assert models.ReceiptToNutritionUser.objects.count() == 0
+    assert models.Matching.objects.count() == 0
+    assert models.NonFoundMatching.objects.count() == 0
+    assert models.Product.objects.count() == 0
+
+    user = User.objects.create_user(username=TEST_USER_AND_PASSWORD, password=TEST_USER_AND_PASSWORD)
+    r2n_partner = mommy.make(models.ReceiptToNutritionPartner, user=user, name=PARTNER_AND_USER_NAME)
+    r2n_user = mommy.make(models.ReceiptToNutritionUser, r2n_partner=r2n_partner, r2n_username=PARTNER_AND_USER_NAME)
+    test_product = mommy.make(models.Product)
+    mommy.make(models.Matching, article_id=ARTICLE_ID_SIMPLE, article_type=ARTICLE_TYPE_SIMPLE, gtin=0,
+               eatfit_product=test_product, price_per_unit=None)
+    mommy.make(models.Matching, article_id=ARTICLE_ID_SIMPLE, article_type=ARTICLE_TYPE_SIMPLE, gtin=1,
+               eatfit_product=test_product, price_per_unit=10)
+
+    BASKET_ANALYSIS_DATA_SIMPLE['r2n_partner'] = r2n_partner.name
+    BASKET_ANALYSIS_DATA_SIMPLE['r2n_username'] = r2n_user.r2n_username
+
+    assert User.objects.count() == 1
+    assert models.ReceiptToNutritionPartner.objects.count() == 1
+    assert models.ReceiptToNutritionUser.objects.count() == 1
+    assert models.Matching.objects.count() == 2
+    assert models.Product.objects.count() == 1
+
+    api_client = APIRequestFactory()
+    view = views.SendReceiptsView.as_view()
+    url = reverse(URL_REVERSE)
+    request = api_client.post(url, BASKET_ANALYSIS_DATA_SIMPLE, format='json')
+    force_authenticate(request, user=user)
+    response = view(request)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert models.NonFoundMatching.objects.count() == 0
+
+    expected_results = models.Matching.objects.filter(article_id=ARTICLE_ID_SIMPLE,
+                                                      article_type=ARTICLE_TYPE_SIMPLE,
+                                                      price_per_unit__isnull=False).annotate(
+        absolute_price_difference=Func(F('price_per_unit') - PRICE, function='ABS')).order_by(
+        'absolute_price_difference').first()
+
+    assert expected_results != models.Matching.objects.filter().first()
+    assert expected_results.gtin == 1
+
+
+@pytest.mark.django_db
+def test_basket_analysis_api_matching_multiple_by_price():
+    assert User.objects.count() == 0
+    assert models.ReceiptToNutritionPartner.objects.count() == 0
+    assert models.ReceiptToNutritionUser.objects.count() == 0
+    assert models.Matching.objects.count() == 0
+    assert models.NonFoundMatching.objects.count() == 0
+    assert models.Product.objects.count() == 0
+
+    user = User.objects.create_user(username=TEST_USER_AND_PASSWORD, password=TEST_USER_AND_PASSWORD)
+    r2n_partner = mommy.make(models.ReceiptToNutritionPartner, user=user, name=PARTNER_AND_USER_NAME)
+    r2n_user = mommy.make(models.ReceiptToNutritionUser, r2n_partner=r2n_partner, r2n_username=PARTNER_AND_USER_NAME)
+    test_product = mommy.make(models.Product)
+    mommy.make(models.Matching, article_id=ARTICLE_ID_SIMPLE, article_type=ARTICLE_TYPE_SIMPLE, gtin=0,
+               eatfit_product=test_product, price_per_unit=10)
+    mommy.make(models.Matching, article_id=ARTICLE_ID_SIMPLE, article_type=ARTICLE_TYPE_SIMPLE, gtin=1,
+               eatfit_product=test_product, price_per_unit=5)
+
+    BASKET_ANALYSIS_DATA_SIMPLE['r2n_partner'] = r2n_partner.name
+    BASKET_ANALYSIS_DATA_SIMPLE['r2n_username'] = r2n_user.r2n_username
+
+    assert User.objects.count() == 1
+    assert models.ReceiptToNutritionPartner.objects.count() == 1
+    assert models.ReceiptToNutritionUser.objects.count() == 1
+    assert models.Matching.objects.count() == 2
+    assert models.Product.objects.count() == 1
+
+    api_client = APIRequestFactory()
+    view = views.SendReceiptsView.as_view()
+    url = reverse(URL_REVERSE)
+    request = api_client.post(url, BASKET_ANALYSIS_DATA_SIMPLE, format='json')
+    force_authenticate(request, user=user)
+    response = view(request)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert models.NonFoundMatching.objects.count() == 0
+
+    expected_results = models.Matching.objects.filter(article_id=ARTICLE_ID_SIMPLE,
+                                                      article_type=ARTICLE_TYPE_SIMPLE).annotate(
+        absolute_price_difference=Func(F('price_per_unit') - PRICE, function='ABS')).order_by(
+        'absolute_price_difference').first()
+
+    assert expected_results != models.Matching.objects.filter().first()
+    assert expected_results.gtin == 1
 
 
 @pytest.mark.django_db
@@ -454,10 +600,10 @@ def test_basket_analysis_api_is_valid():
     response = view(request)
 
     expected_results = {'nutri_score_by_basket': [
-        {'receipt_id': '1551533421', 'receipt_datetime': '2019-03-02T14:30:21', 'business_unit': 'Migros',
+        {'receipt_id': '1551533421', 'receipt_datetime': '2019-03-02T14:30:21Z', 'business_unit': 'Migros',
          'nutri_score_average': 'C', 'nutri_score_indexed': 2.96}], 'nutri_score_by_week': [
         {'name_calendar_week': '2019-08', 'nutri_score_average': 'C', 'nutri_score_indexed': 2.96,
-         'start_date': '2019-02-25', 'end_date': '2019-03-04'}], 'improvement_potential': [
+         'start_date': '2019-02-25T00:00:00Z', 'end_date': '2019-03-04T00:00:00Z'}], 'improvement_potential': [
         {'nutrient': 'saturatedFat', 'ofcom_point_average': 4.6, 'potential_percentage': 20.19, 'amount': 129.56,
          'unit': 'g', 'sources': [{'minor_category_id': 84, 'amount': 46.0, 'unit': 'g'},
                                   {'minor_category_id': 82, 'amount': 31.12, 'unit': 'g'},
